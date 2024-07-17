b9e860f604d1647e506180d487d69755fe261a9b
marc4data <marc4data@gmail.com>
2024-07-16 23:17:14 -0700

    Finishing Tues Dev
    More top-level graphs and improved plot layout.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # usa-inbound_border_crossing
+
+Exploring some data about Inbound Border Crossings into the US over the last couple of decades.  Data is curated and publicly available by the Bureau of Transportation Statistics.  
---
+++ scripts_r/Faceting Graphs in Markdown.R
@@ -0,0 +1,14 @@
+library(ggplot2)
+p1 <- qplot(mpg, wt, data = mtcars, colour = cyl)
+p2 <- qplot(mpg, data = mtcars) + ggtitle("title")
+p3 <- qplot(mpg, data = mtcars, geom = "dotplot")
+p4 <-
+  p1 + facet_wrap( ~ carb, nrow = 1) + theme(legend.position = "none") +
+  ggtitle("facetted plot")
+
+p1
+p2
+p3
+p4
+
+grid.arrange(p1, p2, nrow = 1)
